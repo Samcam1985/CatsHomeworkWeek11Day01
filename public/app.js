54d43891abbd8348a709cab97ba39e168afc83a2
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-31 19:58:12 +0100

    Cats removed from HTML and added to javascript - first extension

--- public/app.js
+++ public/app.js
@@ -46,8 +46,14 @@ cats.appendChild(ul)
 };
 
 var app = function() {
-  addCat("Gemini", "Beans", "http://www.catadoption.co.uk/wp-content/uploads/2013/07/ea4c22547edfcf6b06c5c0bec38e3800.jpg");
+  addCat("Vigo", "Cat Soup", "https://devblogs.nvidia.com/parallelforall/wp-content/uploads/2016/07/cute.jpg");
+  addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+  addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+  addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg")
+
 }
+
+
 
 
 // var app = function () {
